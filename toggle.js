37b5e3d1ed4c26b66ccca1875a35a56fa9a11f8f
omya1334 <126615491+omya1334@users.noreply.github.com>
2023-03-01 22:05:12 +0530

    shree ram

---
+++ toggle.js
@@ -0,0 +1,7 @@
+var sidebar1= document.querySelector(".sidebar").querySelectorAll("li")
+
+sidebar1.forEach(element =>{
+    element.addEventListener('click',function(){
+        sidebar1.forEach(sidebar.classList.toggle('showsidebar'))
+    })
+})
